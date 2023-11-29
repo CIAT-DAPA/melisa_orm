5ded20f72ce71cc16505298cffa0cd77be285b32
CarlosNasayo <CarlosNasayo@users.noreply.github.com>
2023-11-29 19:22:01 +0000

    Update version to v0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="modelswp",
-    version='v1.0.0',
+    version='v0.0.1',
     author="CarlosNasayo",
     author_email="c.sotelo@cgiar.com",
     description="orm for mellisa",
