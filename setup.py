f6a5a33c18c888a152fec570e3438ff8cbe1925e
CarlosNasayo <CarlosNasayo@users.noreply.github.com>
2023-11-30 20:43:54 +0000

    Update version to v1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="melisa_orm",
-    version='v1.0.0',
+    version='v1.0.2',
     author="stevensotelo",
     author_email="h.sotelo@cgiar.com",
     description="orm for mellisa",
