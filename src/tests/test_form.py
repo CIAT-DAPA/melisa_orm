f0668f268d0fe66a20fcc2a6a68f542b108c738a
stevensotelo <stevenbetancurt@hotmail.com>
2023-11-30 15:37:49 -0500

    fixed the test cases

--- src/tests/test_form.py
+++ src/tests/test_form.py
@@ -7,8 +7,8 @@ from datetime import datetime
 dir_path = os.path.dirname(os.path.realpath(__file__))
 orm_dir_path = os.path.abspath(os.path.join(dir_path, '..'))
 sys.path.append(orm_dir_path)
-from melisaORM.models.form import Form
-from melisaORM.models.track import Track
+from melisa_orm.models.form import Form
+from melisa_orm.models.track import Track
 
 class TestForm(unittest.TestCase):
 
