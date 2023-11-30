cbce642e77d56422ad0edd2ecb53fd75037b45fa
stevensotelo <stevenbetancurt@hotmail.com>
2023-11-30 15:34:21 -0500

    change the name of package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ormMelisa",
+    name="melisa_orm",
     version='v1.0.0',
-    author="Stevensotelo",
-    author_email="c.sotelo@cgiar.com",
+    author="stevensotelo",
+    author_email="h.sotelo@cgiar.com",
     description="orm for mellisa",
     url="https://github.com/CIAT-DAPA/melisa_orm",
     download_url="https://github.com/CIAT-DAPA/melisa_orm",
