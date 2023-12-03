c8638f03236c2b66607f0e19a27150efdb7f9d3c
stevensotelo <stevensotelo@users.noreply.github.com>
2023-12-03 21:00:15 +0000

    Update version to v1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="melisa_orm",
-    version='v1.0.2',
+    version='v1.0.3',
     author="stevensotelo",
     author_email="h.sotelo@cgiar.com",
     description="orm for mellisa",
