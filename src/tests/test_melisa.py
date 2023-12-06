717da54ca0036265cd1789a7aed712b96e08fc10
CarlosNasayo <carlosnasayov@gmail.com>
2023-12-06 11:07:25 -0500

    added track to melisa collection

--- src/tests/test_melisa.py
+++ src/tests/test_melisa.py
@@ -2,11 +2,13 @@ import sys
 import unittest
 import os
 import mongomock
+from datetime import datetime
 from mongoengine import *
 dir_path = os.path.dirname(os.path.realpath(__file__))
 orm_dir_path = os.path.abspath(os.path.join(dir_path, '..'))
 sys.path.append(orm_dir_path)
 from melisa_orm.models.melisa import Melisa
+from melisa_orm import Track
 
 
 class TestMelisa(unittest.TestCase):
@@ -15,13 +17,21 @@ class TestMelisa(unittest.TestCase):
         disconnect()
         connect('mongoenginetest', host='mongodb://localhost', mongo_client_class=mongomock.MongoClient)
    
+        self.track = Track(
+            user='user',
+            created=datetime.now(),
+            updated=datetime.now(),
+            enable=True
+        )
         self.melisa = Melisa(
             name='melissa_name',
             url_post='https://www.ejemplo.com',
             token='57eueheh',
             say_hi=True,
             say_wait=True,
-            countries=['colombia','panama']
+            countries=['colombia','panama'],
+            track=self.track
+
         )
     def tearDown(self):
         
